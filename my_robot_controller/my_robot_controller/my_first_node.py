c14d7a8d7433aeb483f8cd2a5e706e8f238aa08d
Túlio Santos Resende <tulio_resende@outlook.com>
2024-04-28 00:12:45 -0300

    teste

---
+++ my_robot_controller/my_robot_controller/my_first_node.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node #importa as funções do Node
+
+
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__('first_node') #O super serve para herdar as funções da classe
+        self.counter_ = 0
+
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info('Hello ' + str(self.counter_))
+        self.counter_ += 1 
+
+def main(arg = None):
+    rclpy.init(args=arg)
+
+    node = MyNode() #Mantem o nó vivo (precisa matar com CTRL + C no terminal)
+    rclpy.spin(node)
+    rclpy.shutdown() #Mata o nó
+
+if __name__ == '__main__':
+    main()
