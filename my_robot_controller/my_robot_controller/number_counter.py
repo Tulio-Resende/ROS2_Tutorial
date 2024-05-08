ba0845c806946f167312129cd955ea3da1bdd46d
Túlio Santos Resende <tulio_resende@outlook.com>
2024-05-07 23:10:49 -0300

    Number Counter Node has been modified

--- my_robot_controller/my_robot_controller/number_counter.py
+++ my_robot_controller/my_robot_controller/number_counter.py
@@ -3,6 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int64
+from std_srvs.srv import SetBool
 
 
 class NumberCounterNode(Node):
@@ -11,8 +12,16 @@ class NumberCounterNode(Node):
         super().__init__("number_counter")
         self.number_counter_publisher_ = self.create_publisher(Int64, "number_count", 10)
         self.number_counter_callback_ = self.create_subscription(Int64,"/number",self.number_counter_callback, 10)
+        self.server_ = self.create_service(SetBool,"reset_count",self.callback_reset_number_counter)
         self.get_logger().info("Number Counter Node has been started")
 
+    def callback_reset_number_counter(self, request, response):
+        self.get_logger().info("teste aqui")
+        if request.data == True:
+            self.counter = 0
+            response.success = True
+        return response
+    
     def number_counter_callback(self, number: Int64):
         msg = Int64()
         self.counter += number.data
